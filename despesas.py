79886eb8f4d2e3068b5381d4e8762cc2b163da0d
victo <victor.akim.ferrer@gmailcom>
2026-09-25 15:26:10 -0300

    agora não tem despesas iniciais

--- despesas.py
+++ despesas.py
@@ -23,21 +23,5 @@ def salvar_despesas():
     with open(ARQUIVO, "w", encoding="utf-8") as arquivo:
         json.dump(dados, arquivo, ensure_ascii=False, indent=4)
 
-despesas = [
-    {
-        'descricao': 'Mensalidade da Faculdade',
-        'tipo': 'fixa',
-        'categoria': 'Educação',
-        'valor': Decimal('1350.00'),
-        'data': '10/xx/xxxx',
-        'forma_pagamento': 'Cartão de crédito'
-    },
-    {
-        'descricao': 'Conta de Luz',
-        'tipo': 'fixa',
-        'categoria': 'Contas Fixas',
-        'valor': Decimal('20.00'),
-        'data': '20/xx/xxxx',
-        'forma_pagamento': 'Cartão de crédito'
-    }
-]
+despesas = carregar_despesas()
+
